15956a3434502a08700d3759439770b30e0a22d7
Ruud <ruud@crashdummy.nl>
2010-06-26 18:58:40 +0200

    Easier log file reading, through the app itself

---
+++ moviemanager/controllers/log.py
@@ -0,0 +1,29 @@
+from moviemanager.lib.base import BaseController, render
+from pylons import request, config, tmpl_context as c
+from pylons.controllers.util import redirect
+from webhelpers.html.builder import literal
+import logging
+import os
+
+cron = config.get('pylons.app_globals').cron
+log = logging.getLogger(__name__)
+
+class LogController(BaseController):
+    """ Show some log stuff """
+
+    def __before__(self):
+        self.setGlobals()
+
+    def index(self):
+        '''
+        See latest log file
+        '''
+
+        c.file = 'MovieManager.log'
+        if int(request.params.get('nr')) > 0:
+            c.file += '.'+request.params.get('nr')
+
+        f = open(os.path.join(os.path.abspath(os.path.curdir), 'logs', c.file), 'r')
+        c.log = literal(f.read().replace('\n', '<br />\n'))
+
+        return render('/log/index.html')
