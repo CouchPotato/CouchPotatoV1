e6b0e56eefa8db8c989cb644cba6d349296b733c
Ruud <ruud@crashdummy.nl>
2010-07-12 23:36:40 +0200

    Log clear

--- app/controllers/log.py
+++ app/controllers/log.py
@@ -1,12 +1,13 @@
-from app.controllers import BaseController
+from app.controllers import BaseController, url, redirect
+from string import ascii_letters, digits
 import cherrypy
 import logging
 import os
-from string import ascii_letters, digits
 
 log = logging.getLogger(__name__)
 file = 'MovieManager.log'
-logfile = os.path.join(os.path.abspath(os.path.curdir), 'logs', file)
+logdir = os.path.join(os.path.abspath(os.path.curdir), 'logs')
+logfile = os.path.join(logdir, file)
 
 class LogController(BaseController):
     """ Show some log stuff """
@@ -28,7 +29,22 @@ class LogController(BaseController):
         log = f.read()
 
         return self.render({'file':file, 'log':self.toSafeString(log)})
-    
+
+    def clear(self):
+
+        for root, subfiles, filenames in os.walk(logdir):
+            for filename in filenames:
+                file = os.path.join(root, filename)
+                
+                if file == logfile:
+                    with open(logfile, 'w') as f:
+                        f.write('')
+                else:
+                    os.remove(file)
+            
+        return redirect(url(controller = 'log', action = 'index'))
+
+
     def toSafeString(self, string):
         safe_chars = ascii_letters + digits + '_ </\>[]-&?=.,;:+!@#$%^&*()\'"{}\n\t'
         return ''.join([char if char in safe_chars else '' for char in string])
