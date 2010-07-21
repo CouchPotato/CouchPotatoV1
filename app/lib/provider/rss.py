485454b5d8f528a32243fb440c3c7da378144587
Ruud <ruud@crashdummy.nl>
2010-07-21 17:05:02 +0200

    Small fix for search string.

--- app/lib/provider/rss.py
+++ app/lib/provider/rss.py
@@ -17,7 +17,7 @@ class rss:
         string = ''.join((c for c in unicodedata.normalize('NFD', unicode(string)) if unicodedata.category(c) != 'Mn'))
         safe_chars = ascii_letters + digits + ' \''
         r = ''.join([char if char in safe_chars else ' ' for char in string])
-        return re.sub('\s+' , ' ', r).replace('\'', '')
+        return re.sub('\s+' , ' ', r).replace('\'s', '').replace('\'', ' ')
 
 
     def gettextelements(self, xml, path):
