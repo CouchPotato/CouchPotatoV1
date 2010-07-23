7a3167d9b77f2ad18ac62ac55f955912680808d1
Ruud <ruud@crashdummy.nl>
2010-07-23 19:53:58 +0200

    More fixes to baseUrl

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -4,12 +4,11 @@ import cherrypy
 import routes
 
 def url(*args, **kwargs):
+    host = 'http://' + cherrypy.request.headers.get('host')
     base = cherrypy.config.get('config').get('global', 'urlbase')
-    base = '/' + base if base else 'http://'+cherrypy.request.headers.get('host')
-    if len(args) == 1 and len(kwargs) == 0 and type(args[0]) in (str, unicode):
-        return cherrypy.url(args[0], base = base)
-    else:
-        return cherrypy.url(routes.url_for(*args, **kwargs), base = base)
+    base = host + '/' + base if base else host
+
+    return cherrypy.url(routes.url_for(*args, **kwargs), base = base)
 
 def redirect(url):
     raise cherrypy.HTTPRedirect(url)
@@ -32,12 +31,9 @@ class BaseController:
 
     def updateGlobals(self):
         base = cherrypy.config.get('config').get('global', 'urlbase')
+        host = 'http://' + cherrypy.request.headers.get('host') + '/'
 
-        if base:
-            self.globals['baseUrl'] = 'http://'+cherrypy.request.headers.get('host') + '/' + base + '/'
-        else:
-            self.globals['baseUrl'] = 'http://'+cherrypy.request.headers.get('host') + '/'
-
+        self.globals['baseUrl'] = host + base + '/' if base else host
         self.globals['lastCheck'] = self.cron.get('yarr').lastCheck()
         self.globals['nextCheck'] = self.cron.get('yarr').nextCheck()
         self.globals['checking'] = self.cron.get('yarr').isChecking()
