66ba6e5ebf9156579180cace42fc2977180a8fec
alshain <c.vonrueti@project-infcom.ch>
2010-08-01 22:26:09 +0200

    removed indentation

--- CouchPotato.py
+++ CouchPotato.py
@@ -1,29 +1,32 @@
-if __name__ == '__main__':
-    import sys
-    import os
+import sys
+import os
 
-    from app import core
-    from app.core.environment import Environment as env_
-
+from app import core
+from app.core.environment import Environment as env_
 
 
 
-    # Include paths
-    #sys.path.insert(0, path_base)
-    #sys.path.insert(0, os.path.join(path_base, 'library'))
 
-    #log = logging.getLogger(__name__)
+# Include paths
+#sys.path.insert(0, path_base)
+#sys.path.insert(0, os.path.join(path_base, 'library'))
 
-    #initDb()
-    #core._env.cfg.get('global', 'server_config')
-    #core._env.loadConfig()
-    core.bootstrap()
-    web_boot = core.frontend.Bootstraper()
-    web_boot.registerStaticDir(
-        '/', '', env_.get('appDir')
-    )
-    web_boot.registerStaticDir('/media', env_.get('appDir'), 'media')
-    web_boot.registerStaticDir(
-        '/cache', env_.get('dataDir'), 'cache'
-    )
+#log = logging.getLogger(__name__)
+
+#initDb()
+#core._env.cfg.get('global', 'server_config')
+#core._env.loadConfig()
+#core.bootstrap()
+
+web_boot = core.frontend.Bootstraper()
+web_boot.registerStaticDir(
+    '/', '', env_.get('appDir')
+)
+web_boot.registerStaticDir('/media', env_.get('appDir'), 'media')
+web_boot.registerStaticDir(
+    '/cache', env_.get('dataDir'), 'cache'
+)
+
+frontend = core.frontend.Frontend()
+frontend.start()
 
