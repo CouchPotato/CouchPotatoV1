940680861cb5e282ba70ae0c419eeea0791879ee
alshain <c.vonrueti@project-infcom.ch>
2010-08-01 19:26:41 +0200

    Improved bootstrapping, now creates directories, parses options

--- app/core/bootstrapper.py
+++ app/core/bootstrapper.py
@@ -1,25 +1,28 @@
 from .environment import Environment as env_
 from optparse import OptionParser
 import sys, os
+from app.config.main import Main as Config
 
 class Bootstrapper(object):
     '''
     classdocs
     '''
+    DEFAULT_DATA_DIR = 'data'
 
 
-    def __init__(self, config = 'data/config.ini'):
+    def __init__(self):
         '''
         Constructor
         '''
         self.detectExeBuild()
-        self.detectRunDir()
+        self.detectAppDir()
         # Define path based on frozen state
-
         # Include paths
-        sys.path.insert(0, env_.get('runDir'))
-        sys.path.insert(0, os.path.join(env_.get('runDir'), 'library'))
-
+        sys.path.insert(0, env_.get('appDir'))
+        sys.path.insert(0, os.path.join(env_.get('appDir'), 'library'))
+        self.parseOptions()
+        self.initAppDirs()
+        self.loadConfig()
 
 
     def detectExeBuild(self):
@@ -28,15 +31,15 @@ class Bootstrapper(object):
         except AttributeError:
             env_.frozen = False
 
-    def detectRunDir(self):
-        rundir = os.path.dirname(os.path.abspath(__file__))
+    def detectAppDir(self):
+        appdir = os.path.dirname(os.path.abspath(__file__))
         if env_.get('frozen'):
             #path_base = os.environ['_MEIPASS2']
-            rundir = os.path.dirname(sys.executable)
-        env_._runDir = rundir
+            appdir = os.path.dirname(sys.executable)
+        env_._appDir = appdir
 
     def parseOptions(self):
-        config = env_.get('defaultConfig')
+        data_dir = self.__class__.DEFAULT_DATA_DIR
         p = OptionParser()
         p.add_option('-d', action = "store_true",
                      dest = 'daemonize', help = "Run the server as a daemon")
@@ -51,12 +54,27 @@ class Bootstrapper(object):
         options, args = p.parse_args()
 
         if args.__len__() == 1:
-            config = args[0]
+            data_dir = args[0]
         elif args.__len__() > 1:
-            print ('Invalid argument cound: [config path]')
+            print ('Invalid argument cound: [data directory]')
             sys.exit(1)
-            env_.setBasePath(os.path.dirname(config))
-            config_name = os.path.basename(config)
+        #register path settings to env
+        env_.setDataDir(data_dir) #creates if not exists
+
+        env_._args = args
+        env_._options = options
+
+    def loadConfig(self):
+        env_.cfg = Config()
+
+    def initAppDirs(self):
+        dirs = ('config', 'logs', 'cache')
+        for dir in dirs:
+            dir = os.path.join(env_.get('dataDir'), dir)
+            if not os.path.isdir(dir):
+                os.mkdir(dir)
+
+
 
 
 
