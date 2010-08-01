d466c6a8d06043b3ceba3178063b25ac1bb8b4e9
alshain <c.vonrueti@project-infcom.ch>
2010-08-01 19:25:52 +0200

    Cleanup, now only dataDir

--- app/core/environment.py
+++ app/core/environment.py
@@ -6,31 +6,24 @@ log = logging.getLogger(__name__)
 class Environment:
     _debugStatus = True;
     _log = None
-    _basePath = ""
-    _cfg = None
+    _appDir = ""
+    _dataDir = ""
     _options = None
     _args = None
     _quiet = False
     _version = '0.3.0'
     _build = 19
     _frozen = False
-    _defaultConfig = 'data/config.ini'
+
+    cfg = None
     @staticmethod
     def doDebug():
         return Environment._debugStatus
     @staticmethod
-    def setBasePath(base_path):
-        if not os.path.isdir(base_path):
-            os.mkdir(base_path)
-        Environment._basePath = base_path
-
-    @staticmethod
-    def getBasePath():
-        return Environment._basePath
-
-    @staticmethod
-    def isQuiet():
-        return Environment._quiet
+    def setDataDir(data_dir):
+        if not os.path.isdir(data_dir):
+            os.mkdir(data_dir)
+        Environment._dataDir = data_dir
 
     @staticmethod
     def get(attr):# default = None, set_non_existant = False):
