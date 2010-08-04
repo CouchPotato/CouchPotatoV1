cdfe197e89c643cb33c0ef2c14619565944c3220
alshain <c.vonrueti@project-infcom.ch>
2010-08-04 14:01:25 +0200

    Event [+] addResults() to add elements from an iterable to the current result set.

--- app/lib/event.py
+++ app/lib/event.py
@@ -6,12 +6,28 @@ class Event(object):
         self._results = [[]]
 
     def addResult(self, result):
+        '''append results to the Event'''
         self._results[0].append(result)
 
+    def addResults(self, self, results):
+        '''add elements of an iterable at the end of the results list'''
+        for result in results:
+            self.addResult(result)
+
     def getResult(self, position = 0):
+        """
+        Return the most recent result set.
+        Use the additional parameter position to
+        access an older set of results.
+        """
         return self._results[position]
 
     def setResults(self, results):
+        """
+        introduce a new set of results.
+        this can be useful for filtering results
+        while retaining the previous state.
+        """
         self.results._insert(0, results)
 
 
