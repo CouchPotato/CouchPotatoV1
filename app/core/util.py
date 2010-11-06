4259a843fe47fc85bfd0ceb92dbbdc5ab9f914c6
alshain <c.vonrueti@project-infcom.ch>
2010-11-06 13:41:08 +0100

    ValueObject: Wrap dict in object as readonly with direct access via properties.

--- app/core/util.py
+++ app/core/util.py
@@ -4,3 +4,15 @@ def list_apply_defaults(values, defaults):
     n elements that are not yet defined.
     """
     return values + defaults[len(values):]
+
+class ValueObject(object):
+    """
+    Create object containing attributes
+    without the possibility to change them
+    afterwards.
+    """
+    def __init__(self, a_dict):
+        self._dict = a_dict.copy()
+
+    def __getattr__(self, name):
+        return self._dict[name]
