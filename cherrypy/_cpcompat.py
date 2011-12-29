f20a6f69cde9a7349fd586b84269614cc250a6c3
Ruud <ruud@crashdummy.nl>
2011-12-30 00:31:12 +0100

    Cherrypy update

--- cherrypy/_cpcompat.py
+++ cherrypy/_cpcompat.py
@@ -16,20 +16,28 @@ It also provides a 'base64_decode' function with native strings as input and
 output.
 """
 import os
+import re
 import sys
 
 if sys.version_info >= (3, 0):
+    py3k = True
     bytestr = bytes
     unicodestr = str
     nativestr = unicodestr
     basestring = (bytes, str)
-    def ntob(n, encoding = 'ISO-8859-1'):
+    def ntob(n, encoding='ISO-8859-1'):
         """Return the given native string as a byte string in the given encoding."""
         # In Python 3, the native string type is unicode
         return n.encode(encoding)
-    def ntou(n, encoding = 'ISO-8859-1'):
+    def ntou(n, encoding='ISO-8859-1'):
         """Return the given native string as a unicode string with the given encoding."""
         # In Python 3, the native string type is unicode
+        return n
+    def tonative(n, encoding='ISO-8859-1'):
+        """Return the given string as a native string in the given encoding."""
+        # In Python 3, the native string type is unicode
+        if isinstance(n, bytes):
+            return n.decode(encoding)
         return n
     # type("")
     from io import StringIO
@@ -37,22 +45,38 @@ if sys.version_info >= (3, 0):
     from io import BytesIO as BytesIO
 else:
     # Python 2
+    py3k = False
     bytestr = str
     unicodestr = unicode
     nativestr = bytestr
     basestring = basestring
-    def ntob(n, encoding = 'ISO-8859-1'):
+    def ntob(n, encoding='ISO-8859-1'):
         """Return the given native string as a byte string in the given encoding."""
         # In Python 2, the native string type is bytes. Assume it's already
         # in the given encoding, which for ISO-8859-1 is almost always what
         # was intended.
         return n
-    def ntou(n, encoding = 'ISO-8859-1'):
+    def ntou(n, encoding='ISO-8859-1'):
         """Return the given native string as a unicode string with the given encoding."""
-        # In Python 2, the native string type is bytes. Assume it's already
-        # in the given encoding, which for ISO-8859-1 is almost always what
-        # was intended.
+        # In Python 2, the native string type is bytes.
+        # First, check for the special encoding 'escape'. The test suite uses this
+        # to signal that it wants to pass a string with embedded \uXXXX escapes,
+        # but without having to prefix it with u'' for Python 2, but no prefix
+        # for Python 3.
+        if encoding == 'escape':
+            return unicode(
+                re.sub(r'\\u([0-9a-zA-Z]{4})',
+                       lambda m: unichr(int(m.group(1), 16)),
+                       n.decode('ISO-8859-1')))
+        # Assume it's already in the given encoding, which for ISO-8859-1 is almost
+        # always what was intended.
         return n.decode(encoding)
+    def tonative(n, encoding='ISO-8859-1'):
+        """Return the given string as a native string in the given encoding."""
+        # In Python 2, the native string type is bytes.
+        if isinstance(n, unicode):
+            return n.encode(encoding)
+        return n
     try:
         # type("")
         from cStringIO import StringIO
@@ -76,7 +100,7 @@ except ImportError:
     # the legacy API of base64
     from base64 import decodestring as _base64_decodebytes
 
-def base64_decode(n, encoding = 'ISO-8859-1'):
+def base64_decode(n, encoding='ISO-8859-1'):
     """Return the native string base64-decoded (as a native string)."""
     if isinstance(n, unicodestr):
         b = n.encode(encoding)
@@ -117,13 +141,13 @@ except NameError:
             i -= 1
             yield x[i]
 
-if sys.version_info >= (3, 0):
+try:
     # Python 3
     from urllib.parse import urljoin, urlencode
     from urllib.parse import quote, quote_plus
     from urllib.request import unquote, urlopen
     from urllib.request import parse_http_list, parse_keqv_list
-else:
+except ImportError:
     # Python 2
     from urlparse import urljoin
     from urllib import urlencode, urlopen
@@ -186,6 +210,18 @@ except ImportError:
     from http.server import BaseHTTPRequestHandler
 
 try:
+    # Python 2. We have to do it in this order so Python 2 builds
+    # don't try to import the 'http' module from cherrypy.lib
+    from httplib import HTTPSConnection
+except ImportError:
+    try:
+        # Python 3
+        from http.client import HTTPSConnection
+    except ImportError:
+        # Some platforms which don't have SSL don't expose HTTPSConnection
+        HTTPSConnection = None
+
+try:
     # Python 2
     xrange = xrange
 except NameError:
@@ -207,20 +243,20 @@ else:
 
 try:
     from email.utils import formatdate
-    def HTTPDate(timeval = None):
-        return formatdate(timeval, usegmt = True)
+    def HTTPDate(timeval=None):
+        return formatdate(timeval, usegmt=True)
 except ImportError:
     from rfc822 import formatdate as HTTPDate
 
-if sys.version_info >= (3, 0):
+try:
     # Python 3
     from urllib.parse import unquote as parse_unquote
-    def unquote_qs(atom, encoding, errors = 'strict'):
-        return parse_unquote(atom.replace('+', ' '), encoding = encoding, errors = errors)
-else:
+    def unquote_qs(atom, encoding, errors='strict'):
+        return parse_unquote(atom.replace('+', ' '), encoding=encoding, errors=errors)
+except ImportError:
     # Python 2
     from urllib import unquote as parse_unquote
-    def unquote_qs(atom, encoding, errors = 'strict'):
+    def unquote_qs(atom, encoding, errors='strict'):
         return parse_unquote(atom.replace('+', ' ')).decode(encoding, errors)
 
 try:
@@ -229,7 +265,7 @@ try:
     json_decode = json.JSONDecoder().decode
     json_encode = json.JSONEncoder().iterencode
 except ImportError:
-    if sys.version_info >= (3, 0):
+    if py3k:
         # Python 3.0: json is part of the standard library,
         # but outputs unicode. We need bytes.
         import json
@@ -280,4 +316,3 @@ except NameError:
     # Python 2
     def next(i):
         return i.next()
-
