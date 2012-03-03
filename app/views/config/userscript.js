7a0c13acf678670dcab38de90118d119f94b3fc6
cienislaw <cienislaw@post.pl>
2012-03-03 11:14:55 +0100

    filmweb code updated according to site changes all tabs to spaces

--- app/views/config/userscript.js
+++ app/views/config/userscript.js
@@ -11,7 +11,7 @@
 // @include     http://whiwa.net/stats/movie/*
 // @include     http://trailers.apple.com/trailers/*
 // @include     http://www.themoviedb.org/movie/*
-// @include 	http://www.allocine.fr/film/* 
+// @include     http://www.allocine.fr/film/* 
 // @include     http://trakt.tv/movie/*
 // @include     http://*.trak.tv/movie/*
 // @include     http://www.rottentomatoes.com/m/*
@@ -21,8 +21,8 @@
 // @include     http://sratim.co.il/view.php?*
 // @exclude     http://trak.tv/movie/*/*
 // @exclude     http://*.trak.tv/movie/*/*
-// @include	http://*.filmweb.pl/*
-// @include	http://filmweb.pl/*
+// @include     http://*.filmweb.pl/*
+// @include     http://filmweb.pl/*
 // ==/UserScript==
 
 var version = 9;
@@ -424,7 +424,7 @@ tmdb = (function(){
      *      Returns TMDB id from the url.
      */
     function getTmdbId() {
-	var tmdb_id = location.href.match(/movie\/(\d+)/g)[0].substr(6);
+        var tmdb_id = location.href.match(/movie\/(\d+)/g)[0].substr(6);
         return tmdb_id;
     }
 
@@ -538,41 +538,42 @@ youtheater = (function(){
 filmweb = (function(){
     var obj = this;
 
-	function isMovie(){
-		var filmType = document.getElementById('filmType').innerText
-		return filmType != '[serial TV]';
-	}
+    function isMovie(){
+        var filmType = document.getElementById('filmType').innerHTML;
+        // 0 movie, 1 tv movie, 2 tv series
+        return filmType < 2;
+    }
 
-	function getName(){
-		// polish or original title if there is no polish one
-		var title = document.getElementsByTagName('title')[0].text.match(/^(.+) \(\d{4}\) (- .+ )?- Filmweb$/)[1];
-		var titleLen = title.length;
-		var metas = document.getElementsByTagName('meta');
-		obj = null;
-		for (i = 0; i < metas.length; i ++) {
-			obj = metas[i];
-			if (obj.content.length >= titleLen) {
-				if (obj.content == title) {
-					break;
-				} else if (obj.content.substr(titleLen,  3) == ' / ') {
-					// original title exists
-					title = obj.content.match(/^.+ \/ (.+)$/)[1];
-					break;
-				}
-			}
-		}
-		// adding 'The' from back to front
-		var theTitle = title.match(/^(.+), The$/);
-		if (theTitle != null) {
-			title = 'The ' + theTitle[1];
-		}
-		return title;
-	}
+    function getName(){
+        // polish or original title if there is no polish one
+        var title = document.getElementsByTagName('title')[0].text.match(/^(.+) \(\d{4}\) (- .+ )?- Filmweb$/)[1];
+        var titleLen = title.length;
+        var metas = document.getElementsByTagName('meta');
+        obj = null;
+        for (i = 0; i < metas.length; i ++) {
+            obj = metas[i];
+            if (obj.content.length >= titleLen) {
+                if (obj.content == title) {
+                    break;
+                } else if (obj.content.substr(titleLen,  3) == ' / ') {
+                    // original title exists
+                    title = obj.content.match(/^.+ \/ (.+)$/)[1];
+                    break;
+                }
+            }
+        }
+        // adding 'The' from back to front
+        var theTitle = title.match(/^(.+), The$/);
+        if (theTitle != null) {
+            title = 'The ' + theTitle[1];
+        }
+        return title;
+    }
 
-	function getYear(){
-        return document.getElementById('filmYear').innerHTML;
-//		return document.getElementsByTagName('title')[0].text.match(/^.+ \((\d{4})\) (- .+ )?- Filmweb$/)[1];
-	}
+    function getYear(){
+        return document.getElementById('filmYear').innerHTML.match(/^ \((\d{4})\) (TV)?$/)[1];
+//      return document.getElementsByTagName('title')[0].text.match(/^.+ \((\d{4})\) (- .+ )?- Filmweb$/)[1];
+    }
 
     function constructor(){
         if(isMovie()){
@@ -597,7 +598,7 @@ filmweb = (function(){
         "rottentomatoes.com" : rotten,
         "youtheater.com": youtheater,
         "sratim.co.il": youtheater,
-	"filmweb.pl": filmweb
+        "filmweb.pl": filmweb
     };
     
     for (var i in factory){
